185beca630d9f892d959ab4004b21b3bfcb7f8b6
Vasily <vasilin97@gmail.com>
2026-05-01 20:01:32 -0700

    Revert "Cycle 604: cut-aggregate API for unital bSeriesConvAug associativity"
    This reverts commit b1eda13e4a87687caf4dd2e4fdcc4ca1d9fa6dc6.

--- OpenMath/ButcherGroup.lean
+++ OpenMath/ButcherGroup.lean
@@ -9,7 +9,6 @@ import OpenMath.ButcherGroup.Section384SlicesQuadMixed
 import OpenMath.ButcherGroup.Section386Conv
 import OpenMath.ButcherGroup.Section386Aug
 import OpenMath.ButcherGroup.Section386Aug.DepthThree
-import OpenMath.ButcherGroup.Section386Aug.CutAssoc
 import OpenMath.ButcherGroup.QuotEquivSlices
 
 /-!
